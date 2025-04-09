4f25729e31ff24090738a43e033652e484372c5f
Manjila Singh <singh.manjila02@gmail.com>
2025-04-09 16:25:59 +0100

    Initial commit

---
+++ src/pages/Data-Preprocess.js
@@ -0,0 +1,10 @@
+import Header from '../components/Header';
+
+export default function DataPreprocess() {
+    return (
+        <>
+            <Header />
+            <h2>This is data Preprocess page</h2>
+        </>
+    )
+}
