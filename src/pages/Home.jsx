666cf9202e2d78241dea9d1ac750aa030cc5a310
Manjila Singh <singh.manjila02@gmail.com>
2025-04-30 22:12:20 +0100

    Added logos

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -38,18 +38,66 @@ const Home = () => {
           <source src={waterVideo} type="video/mp4" />
           Your browser does not support the video tag.
         </video>
+        <div className="absolute inset-0 bg-black bg-opacity-20"></div>
         <div className="container mx-auto px-4 text-center relative z-10" data-aos="fade-up" data-aos-delay="200">
-          <h1 className="text-7xl font-bold uppercase mb-6" data-aos="fade-up" data-aos-delay="400">NextGen In A Box</h1>
-          <p className="text-4xl mb-12" data-aos="fade-up" data-aos-delay="600">Transforming Water Modeling in Minutes</p>
-          <div className="space-x-6" data-aos="fade-up" data-aos-delay="800">
-            <HashLink smooth to="/#portfolio" className="btn-primary text-xl px-12 py-4">
-              Our Tools
-            </HashLink>
-            <a className="btn-primary text-xl px-12 py-4" href="http://docs.ciroh.org/training-NGIAB-101/" target="_blank"
-    rel="noopener noreferrer">Getting started</a>
-          </div>  
+          <div className="flex flex-col items-center justify-center space-y-12">
+            <img 
+              src="/assets/img/logos/ciroh_dark.png" 
+              alt="CIROH Logo" 
+              className="w-48 md:w-56 mb-4 transform transition-all duration-700 hover:scale-110"
+              data-aos="zoom-in"
+              data-aos-delay="300"
+            />
+            <h1 className="text-7xl font-bold uppercase mb-6" data-aos="fade-up" data-aos-delay="400">NextGen In A Box</h1>
+            <p className="text-4xl mb-12" data-aos="fade-up" data-aos-delay="600">Transforming Water Modeling in Minutes</p>
+            <div className="space-x-6" data-aos="fade-up" data-aos-delay="800">
+              <HashLink smooth to="/#portfolio" className="btn-primary text-xl px-12 py-4">
+                Our Tools
+              </HashLink>
+              <a className="btn-primary text-xl px-12 py-4" href="http://docs.ciroh.org/training-NGIAB-101/" target="_blank" rel="noopener noreferrer">
+                Getting started
+              </a>
+            </div>
+          </div>
         </div>
       </header>
+
+      {/* Partners Logo Carousel */}
+      <section className="bg-white py-12 overflow-hidden">
+        <div className="container mx-auto">
+          <div className="relative overflow-hidden w-full">
+            <div className="animate-carousel">
+              <div className="flex items-center space-x-24 mx-8">
+                <img 
+                  src="/assets/img/logos/lynker-light.jpeg" 
+                  alt="Lynker Logo" 
+                  className="h-16 md:h-22 object-contain inline-block" 
+                />
+                <img 
+                  src="/assets/img/logos/byu.png" 
+                  alt="BYU Logo" 
+                  className="h-16 md:h-20 object-contain inline-block" 
+                />
+                <img 
+                  src="/assets/img/logos/aws.png" 
+                  alt="AWS Logo" 
+                  className="h-16 md:h-20 object-contain inline-block" 
+                />
+                <img 
+                  src="/assets/img/logos/noaa.png" 
+                  alt="NOAA Logo" 
+                  className="h-16 md:h-20 object-contain inline-block" 
+                />
+                <img 
+                  src="/assets/img/logos/awi.png" 
+                  alt="AWI Logo" 
+                  className="h-16 md:h-20 object-contain inline-block" 
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
 
       {/* About Section */}
 
